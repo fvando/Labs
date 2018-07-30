27b505de0fed61f0157259ea08c5ea7e9cfba657
vando.moreira <fvando@gmail.com>
2018-07-30 21:48:14 +0100

    Ajustes

--- csT/csT/Program.cs
+++ csT/csT/Program.cs
@@ -11,30 +11,24 @@ namespace csT
         static void Main(string[] args)
         {
 
-            int _k = 0;
+            //Value K
+            int _k = Convert.ToInt32(Console.ReadLine());;
 
-            _k = Convert.ToInt32(Console.ReadLine());
+            int _array_size = Convert.ToInt32(Console.ReadLine());;
 
+            int[] _array = new int[_array_size];
 
-            int[] res;
+            int _array_item;
 
-            int _elements_size = 0;
-
-            _elements_size = Convert.ToInt32(Console.ReadLine());
-
-            int[] _elements = new int[_elements_size];
-
-            int _elements_item;
-
-            for (int _elements_i = 0; _elements_i < _elements_size; _elements_i++)
+            for (int i = 0; i < _array_size; i++)
             {
-                _elements_item = Convert.ToInt32(Console.ReadLine());
-                _elements[_elements_i] = _elements_item;
+                _array_item = Convert.ToInt32(Console.ReadLine());
+                _array[i] = _array_item;
             }
 
             csT.Cls01 App = new csT.Cls01();
 
-            Console.WriteLine(App.TMInputArray(_elements, _k));
+            Console.WriteLine(App.TMInputArray(_array, _k));
 
             Console.ReadLine();
 
