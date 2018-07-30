ed5baa6d93f3c54c02561253f442e85f4d738fa4
vando.moreira <fvando@gmail.com>
2018-07-30 21:53:21 +0100

    Ajustes

--- csT/csT/Program.cs
+++ csT/csT/Program.cs
@@ -14,20 +14,27 @@ namespace csT
             //Value K
             int _k = Convert.ToInt32(Console.ReadLine());;
 
+            //size array
             int _array_size = Convert.ToInt32(Console.ReadLine());;
 
             int[] _array = new int[_array_size];
 
             int _array_item;
 
+
+            //Array elements 
             for (int i = 0; i < _array_size; i++)
             {
                 _array_item = Convert.ToInt32(Console.ReadLine());
                 _array[i] = _array_item;
             }
 
+
+            //Classe new instance
             csT.Cls01 App = new csT.Cls01();
 
+
+            //Output array
             Console.WriteLine(App.TMInputArray(_array, _k));
 
             Console.ReadLine();
